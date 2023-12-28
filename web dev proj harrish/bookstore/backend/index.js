c41d1451f8e19d1bf0188c0bb956fe7e23ea8df9
HArrishArun <harrisharun2003@gmail.com>
2023-12-28 15:16:48 +0530

    end-of-routes

--- web dev proj harrish/bookstore/backend/index.js
+++ web dev proj harrish/bookstore/backend/index.js
@@ -1,7 +1,8 @@
 import express, { response } from "express";
 import {PORT,mongodburl} from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookMoedl.js";
+
+import booksRoute from './routes/booksRoute.js'
 
 const app=express();
 app.use(express.json())
@@ -10,42 +11,8 @@ app.get('/',(request,response)=>{
     return response.status(234).send("welcome")
 });
 
-app.post('/books',async(request,response)=>{
-    try{
-        if(!request.body.title||!request.body.author||!request.body.publishYear){
-            return response.status(400).send({message:'send all req fields title,author,publishyear'});
-        }
-        const newbook={
-            title:request.body.title,
-            author:request.body.author,
-            publishYear:request.body.publishYear
-        };
-        const book =await Book.create(newbook);
-        return response.status(201).send(book);
-    }
-    catch(error){
-        console.log(error.message);
-        response.status(500)
 
-
-    }
-});
-app.get('/books',async(request,response)=>{
-
-
-    try{
-        const book=await Book.find({});
-        return response.status(200).json(books);
-    }
-    catch(error){
-        console.log(error.message);
-        response.status(500)
-
-
-    }
-});
-
-
+app.use('/books',booksRoute);
 mongoose.connect(mongodburl)
 .then(()=>{
     console.log("App connected to db")
---
+++ web dev proj harrish/bookstore/backend/routes/booksRoute.js
@@ -0,0 +1,108 @@
+import express from "express";
+import { Book } from "../models/bookMoedl.js";
+const router=express.Router();
+
+router.post('/',async(request,response)=>{
+    try{
+        if(!request.body.title||!request.body.author||!request.body.publishYear){
+            return response.status(400).send({message:'send all req fields title,author,publishyear'});
+        }
+        const newbook={
+            title:request.body.title,
+            author:request.body.author,
+            publishYear:request.body.publishYear
+        };
+        const book =await Book.create(newbook);
+        return response.status(201).send(book);
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500)
+
+
+    }
+});
+router.get('/',async(request,response)=>{
+
+
+    try{
+        const books=await Book.find({});
+        return response.status(200).json({
+            count:books.length,
+            data:books
+        });
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500)
+
+
+    }
+});//for getting all books
+
+
+//:id is for getting books by id
+router.get('/:id',async(request,response)=>{
+
+
+    try{
+        const{id}=request.params;
+        const book=await Book.findById(id)
+        return response.status(200).json({
+            count:book.length,
+            data:book
+        });
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500)
+
+
+    }
+});
+
+//route for updating a book
+
+router.put('/:id', async (request, response) => {
+    try {
+      const { title, author, publishYear } = request.body;
+      
+      if (!title || !author || !publishYear) {
+        return response.status(400).json({ message: 'Send all required fields: title, author, publishYear' });
+      }
+  
+      const { id } = request.params;
+      const result = await Book.findByIdAndUpdate(id, request.body, { new: true });
+  
+      if (!result) {
+        return response.status(404).json({ message: 'Book not found' });
+      }
+  
+      return response.status(200).json({ message: 'Book updated successfully'});
+    } catch (error) {
+      console.error(error.message);
+      return response.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
+//delete a book
+
+router.delete('/:id',async(request,response)=>{
+
+
+    try{
+       const{id}=request.params;
+       const result=await Book.findByIdAndDelete(id);
+       if(!result){
+        return response.status(404).json({message:'book not found'});
+       }
+       return response.status(200).send({message:'book deleted successfully'});
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500)
+
+
+    }
+});
+export default router;
