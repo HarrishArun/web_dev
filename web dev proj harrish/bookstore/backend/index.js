96f6fc9862ebe4829953e0af91e205f5257fcb72
HArrishArun <harrisharun2003@gmail.com>
2023-12-28 16:21:12 +0530

    end-book-list

--- web dev proj harrish/bookstore/backend/index.js
+++ web dev proj harrish/bookstore/backend/index.js
@@ -3,9 +3,24 @@ import {PORT,mongodburl} from "./config.js";
 import mongoose from "mongoose";
 
 import booksRoute from './routes/booksRoute.js'
+import cors from 'cors';
 
 const app=express();
-app.use(express.json())
+app.use(express.json());
+//moddleware to handle cors policy
+
+//option1:applw all orgins with default of cors app.use(cors())
+
+app.use(cors());
+//option 2 allow custom orgins
+
+// app.use((
+//     cors({
+//         origin:'http://localhost:300',
+//         methods:['GET','POST','PUT','DELETE'],
+//         allowedHeaders:['Content-Type']
+//     })
+// ))
 app.get('/',(request,response)=>{
     console.log(request)
     return response.status(234).send("welcome")
