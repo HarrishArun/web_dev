5aced444bac7cf196a6b605e057e99f403f1857a
HArrishArun <harrisharun2003@gmail.com>
2023-12-28 21:24:24 +0530

    end-course

--- web dev proj harrish/bookstore/frontend/src/page/DeleteBook.jsx
+++ web dev proj harrish/bookstore/frontend/src/page/DeleteBook.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import BackButton from '../../components/BackButton';
+import Spinner from '../../components/spinners';
+import axios from 'axios';
+import { useNavigate,useParams } from 'react-router-dom';
+import { BsHandbagFill } from 'react-icons/bs';
+
 
 const DeleteBook = () => {
+    const [loading,setLoading]=useState(false);
+    const navigate=useNavigate();
+    const{id}=useParams();
+    const handleDeletebook=()=>{
+        setLoading(true);
+        axios
+        .delete(`http://localhost:5555/books/${id}`,data)
+        .then(()=>{
+            setLoading(false);
+            navigate('/');
+        
+        })
+        .catch((error)=>{
+            setLoading(false);
+            alert("error occured");
+            console.log(error);
+        });
+    }
   return (
-    <div>DeleteBook</div>
+    <div className='p-4'><BackButton/>
+    <h1 className='text-3xl my-4'>Delete book</h1>{
+        loading?<Spinner/>:''
+    }<div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] mx-auto'>
+        <h3 className='text-2xl'>Are you sure you want to delete this book</h3>
+
+        <button className='p-4 bg-red-600 text-white m-8 w-full' onClick={handleDeletebook}>Yes</button>
+        </div></div>
   )
 }
 
